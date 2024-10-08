7278e99fd1226b5c120f903d705c08ff30049f70
caillef <corentin.cailleaud@caillef.com>
2024-10-08 13:56:05 +0200

    x

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 cp props/props.lua tmpprops.lua
-echo `ghead -n -2 config/config.lua` `cat props/props.lua` > tmpnewprops.lua
+echo `ghead -n -2 config/config.lua` "\n\n" `cat props/props.lua` > tmpnewprops.lua
 mv tmpnewproprs.lua props/props.lua
 
 git add .
 git commit -m "x"
 git push origin main
 
-cp tmpprops.lua > props/props.lua
+cp tmpprops.lua props/props.lua
 
 # rm tmpprops.lua tmpnewprops.lua
 
