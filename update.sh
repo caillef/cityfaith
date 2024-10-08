ef30c41f4ba93bb13f96a5e0d477480d21408c02
caillef <corentin.cailleaud@caillef.com>
2024-10-08 14:00:01 +0200

    x

--- update.sh
+++ update.sh
@@ -1,14 +1,30 @@
-cp props/props.lua tmpprops.lua
-echo `ghead -n -2 config/config.lua` "\n\n" `cat props/props.lua` > tmpnewprops.lua
-mv tmpnewproprs.lua props/props.lua
+# Function to concat config
+concat_config() {
+    local file="$1"
+    cp "$file" "tmp$file"
+    echo "`ghead -n -2 config/config.lua`" "\n\n" "`cat $file`" > "tmpnew$file"
+    cp "tmpnew$file" "$file"
+}
 
+# Function to revert to previous state
+revert_state() {
+    local file="$1"
+    cp "tmp$file" "$file"
+    rm "tmp$file" "tmpnew$file"
+}
+
+# Concat configs
+concat_config "props/props.lua"
+concat_config "squads/squads.lua"
+
+# Git operations
 git add .
 git commit -m "x"
 git push origin main
 
-cp tmpprops.lua props/props.lua
-
-# rm tmpprops.lua tmpnewprops.lua
+# Revert to previous state
+revert_state "props/props.lua"
+revert_state "squads/squads.lua"
 
 # copy commit id
 git rev-parse HEAD | pbcopy
