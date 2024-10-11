49457e0ee4775f2b19ac6a1b22b31f338882de9d
caillef <corentin.cailleaud@caillef.com>
2024-10-11 14:35:26 +0200

    x

--- world.lua
+++ world.lua
@@ -1,4 +1,4 @@
-local COMMIT_HASH = "cd0bb9e2"
+local COMMIT_HASH = "6c66540c"
 Modules = {
     common = "github.com/caillef/cityfaith/common:" .. COMMIT_HASH,
     gameConfig = "github.com/caillef/cityfaith/config:" .. COMMIT_HASH,
